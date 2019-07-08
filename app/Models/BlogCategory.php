fac546217dd6222ffc36ffb1981e0f920f92e599
VShander <extrazone@yandex.ru>
2019-07-08 21:43:00 +0300

    Add categories relations

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -9,12 +9,41 @@ class BlogCategory extends Model
 {
     use SoftDeletes;
 
-    protected $fillable = [
-        'title',
-        'alias',
-        'parent_id',
-        'description'
-    ];
+    const ROOT = 1;
 
+    protected $fillable
+        = [
+            'title',
+            'alias',
+            'parent_id',
+            'description',
+        ];
+
+    /**
+     * @return \App\Models\BlogCategory|\Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parentCategory()
+    {
+        return $this->belongsTo(BlogCategory::class,
+            'parent_id', 'id');
+    }
+
+    /*
+     *
+     */
+    public function getParentTitleAttribute()
+    {
+        $title = $this->parentCategory->title
+            ?? ($this->isRoot() ? 'Корень' : '???');
+
+        return $title;
+    }
+    /*
+     *
+     */
+    public function isRoot()
+    {
+        return $this->id === BlogCategory::ROOT;
+    }
 
 }
